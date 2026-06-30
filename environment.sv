f44709aab2395d536908ae71520b3382baab8d69
G Rajendra <gr2205002@gmail.com>
2026-06-30 16:05:58 +0530

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -6,16 +6,60 @@
 
 class environment;
 
-  monitor mon;
+  //--------------------------------------------------
+  // Components
+  //--------------------------------------------------
+  generator  gen;
+  driver     drv;
+  monitor    mon;
+  scoreboard scb;
+
+  //--------------------------------------------------
+  // Mailboxes
+  //--------------------------------------------------
+  mailbox #(transaction) gen2drv;
+  mailbox #(transaction) mon2scb;
+
+  //--------------------------------------------------
+  // Virtual Interface
+  //--------------------------------------------------
   virtual riscv_if vif;
 
+  //--------------------------------------------------
+  // Constructor
+  //--------------------------------------------------
   function new(virtual riscv_if vif);
+
     this.vif = vif;
-    mon = new(vif);
+
+    // Create mailboxes
+    gen2drv = new();
+    mon2scb = new();
+
+    // Create components
+    gen = new(gen2drv);
+    drv = new(gen2drv, vif);
+    mon = new(mon2scb, vif);
+    scb = new(mon2scb);
+
   endfunction
 
+  //--------------------------------------------------
+  // Run
+  //--------------------------------------------------
   task run();
-    mon.run();
+
+    $display("---------------------------------------");
+    $display("ENVIRONMENT STARTED");
+    $display("---------------------------------------");
+
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      scb.run();
+    join
+
   endtask
 
 endclass
