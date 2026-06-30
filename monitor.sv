604b727b5fdedd2452f738905e7083933358b9ec
G Rajendra <gr2205002@gmail.com>
2026-06-30 16:07:45 +0530

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,14 +1,87 @@
 class monitor;
 
+  //----------------------------------------------------
+  // Mailbox
+  //----------------------------------------------------
+  mailbox #(transaction) mon2scb;
+
+  //----------------------------------------------------
+  // Virtual Interface
+  //----------------------------------------------------
   virtual riscv_if vif;
 
-  function new(virtual riscv_if vif);
-    this.vif = vif;
+  //----------------------------------------------------
+  // Constructor
+  //----------------------------------------------------
+  function new(mailbox #(transaction) mb,
+               virtual riscv_if vif);
+
+    this.mon2scb = mb;
+    this.vif     = vif;
+
   endfunction
 
+  //----------------------------------------------------
+  // Run
+  //----------------------------------------------------
   task run();
-    repeat (10) @(posedge vif.clk);
-    $display("MONITOR: Simulation running, no DUT peeking");
+
+    transaction tr;
+
+    forever begin
+
+      @(posedge vif.clk);
+
+      if(vif.rst)
+        continue;
+
+      tr = new();
+
+      //-----------------------------
+      // Capture current instruction
+      //-----------------------------
+      tr.instr = vif.instruction;
+
+      //-----------------------------
+      // Decode instruction name
+      //-----------------------------
+      case(vif.instruction[6:0])
+
+        7'b0110011 : tr.instr_name = "ADD/SUB";
+        7'b0010011 : tr.instr_name = "ADDI";
+        7'b0000011 : tr.instr_name = "LW";
+        7'b0100011 : tr.instr_name = "SW";
+        7'b1100011 : tr.instr_name = "BEQ";
+
+        default    : tr.instr_name = "UNKNOWN";
+
+      endcase
+
+      //-----------------------------
+      // Sample Functional Coverage
+      //-----------------------------
+      tr.sample();
+
+      //-----------------------------
+      // Send to Scoreboard
+      //-----------------------------
+      mon2scb.put(tr.copy());
+
+      //-----------------------------
+      // Display
+      //-----------------------------
+      $display("--------------------------------------");
+      $display("MONITOR");
+      $display("--------------------------------------");
+      $display("PC          = %08h",vif.pc);
+      $display("Instruction = %08h",vif.instruction);
+      $display("WB Enable   = %0b",vif.wb_reg_write);
+      $display("WB RD       = %0d",vif.wb_rd);
+      $display("WB DATA     = %08h",vif.wb_data);
+      $display("--------------------------------------");
+
+    end
+
   endtask
 
 endclass
